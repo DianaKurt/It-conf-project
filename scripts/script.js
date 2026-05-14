8fd3abfda611f1e8da5d6661f9bc46aed427cd85
Diana <dialeejee18@gmail.com>
2026-05-14 12:24:57 +0500

    add new scroll plagin and add README

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 gsap.registerPlugin(ScrollToPlugin);
+gsap.registerPlugin(ScrollTrigger); // регистрируем плагин ScrollTrigger.
 
 document.querySelectorAll('.nav-link').forEach(link => { // находим все элементы с классом nav-link.
     link.addEventListener('click', (e) => {
@@ -8,9 +9,26 @@ document.querySelectorAll('.nav-link').forEach(link => { // находим вс�
         if (target && document.querySelector(target)) { // проверяем, существует ли target и является ли он элементом.
             gsap.to(window, { // плавно прокручиваем страницу к элементу с классом nav-link. target - это значение атрибута href.
                 duration: 1,
-                scrollTo: { y: target, offsetY: 5 },
+                scrollTo: { y: target, offsetY: 140 },
                 ease: "power2.out" // эффект плавного перехода.
             });
         }
+    });
+});
+
+
+gsap.from(".header, .hero", { y: -20, opacity: 0, duration: 1, stagger: 0.2 }); // плавно прокручиваем страницу к элементу с классом nav-link.
+
+gsap.utils.toArray(".section-top, .speakers-grid, .advantages-grid, .price-grid").forEach((section )=> {
+    gsap.from(section, {
+        scrollTrigger: {
+            trigger: section,
+            start: "top 80%",
+            toggleActions: "play reverse play reverse",
+        },
+        y: 50,
+        opacity: 0,
+        duration: 0.8,
+        ease: "power2.out",
     });
 });
